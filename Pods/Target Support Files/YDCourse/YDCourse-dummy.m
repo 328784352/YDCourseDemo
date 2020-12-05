b528e741d869f4277448e1e3eeeb40dfee4befef
guquan <guquan@yuuxin.com>
2020-12-05 16:17:13 +0800

    first commit

---
+++ Pods/Target Support Files/YDCourse/YDCourse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YDCourse : NSObject
+@end
+@implementation PodsDummy_YDCourse
+@end
---
+++ YDCourse/YDCourseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  YDCourseViewController.swift
+//  YDCourseDemo
+//
+//  Created by 小权权 on 2020/12/4.
+//
+
+import UIKit
+
+class YDCourseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
